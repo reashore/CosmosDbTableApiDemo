eb32a38eaf4f440c8e0f130e2b2e567fd4090a13
Frank J. Reashore <reashore@telus.net>
2018-08-16 21:07:51 -0700

    Initial commit

---
+++ CosmosDbTableApiDemo/MovieEntity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Azure.CosmosDB.Table;
+
+namespace CosmosDbTableApiDemo
+{
+	public class MovieEntity : TableEntity
+	{
+	    public MovieEntity()
+	    {
+
+	    }
+
+		public MovieEntity(string genre, string movieTitle)
+		{
+			PartitionKey = genre;
+			RowKey = movieTitle;
+		}
+
+		public string Genre => PartitionKey;
+
+		public string Title => RowKey;
+
+		public int Year { get; set; }
+
+		public string Length { get; set; }
+
+		public string Description { get; set; }
+	}
+}
